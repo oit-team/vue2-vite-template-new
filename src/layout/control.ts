9ede4c4210b8bc754683dccaba32cc918780a87e
邱旭 <uxuip@outlook.com>
2022-11-02 18:50:31 +0800

    feat: 缓存是否折叠

--- src/layout/control.ts
+++ src/layout/control.ts
@@ -1,7 +1,18 @@
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 
 export const collapse = ref(false)
 
 export const toggleCollapse = () => {
   collapse.value = !collapse.value
 }
+
+try {
+  const cache = sessionStorage.getItem('collapse')
+  if (cache) collapse.value = !!JSON.parse(cache)
+}
+catch {
+  // ignore
+}
+
+// 缓存值
+watch(collapse, () => sessionStorage.setItem('collapse', JSON.stringify(collapse.value)))
